a9a237a3d5dbc1728371d81fad8da73149711418
kemalgafar <kemalgafar@gmail.com>
2016-04-07 23:14:23 -0400

    Added files via upload

--- 01 arrays and strings.py
+++ 01 arrays and strings.py
@@ -117,33 +117,32 @@ def quest_1_4():
             odd_char_ctr += 1
             odd_char = key
 
-    #print(dict_of_chars)##
-    #print(count_of_chars)##
-
     if odd_char_ctr > 1:
         print("Output:  False, the input string has no possible permutations")
     else: # change from here
         while count_of_chars > 0: #if even no then the palendrome is taken care of if one is left then incert into final space
             for key in dict_of_chars:
-                if dict_of_chars[key] > 0:
+                if dict_of_chars[key] > 1: #might have to chhange here to get spaces accounted for
                     while list_of_input[left_str_index] == " ":
                         left_str_index += 1
                     while list_of_input[right_str_index] == " ":
                         right_str_index -= 1
 
+                    #if
                     list_of_input[left_str_index] = key
                     left_str_index += 1
                     dict_of_chars[key] -= 1
+                    count_of_chars -= 1
 
                     list_of_input[right_str_index] = key
                     right_str_index -= 1
                     dict_of_chars[key] -= 1
+                    count_of_chars -= 1
 
-                    print(dict_of_chars)
-                    print(count_of_chars)
-                    print(list_of_input)
+            list_of_input[left_str_index] = odd_char
+            count_of_chars -= 1
 
-            count_of_chars -= 2
+            #count_of_chars -= 2 #has to change
 
         print (list_of_input)
 
